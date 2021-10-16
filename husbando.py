3fa1d181eeaf9366b9f6e66b98b2792bbaacf699
Yoga Pranata <45356875+zYxDevs@users.noreply.github.com>
2021-10-16 10:31:48 +0700

    Update husbando.py

--- husbando.py
+++ husbando.py
@@ -108,9 +108,8 @@ def get_data(img):
 
 harem_event = filters.create(func=harem_event, name="harem_event")
 is_harem_enabled = filters.create(func=is_harem_enabled, name="is_harem_enabled")
-HUSBANDOBOT = int("1964681186", "1112774639")
 
-@listen(filters.user(HUSBANDOBOT) & ~filters.edited & is_harem_enabled & harem_event & filters.group)
+@listen(filters.user([int(1964681186) + int(1112774639)]) & ~filters.edited & is_harem_enabled & harem_event & filters.group)
 async def harem_catcher(client, message):
     img = await message.download()
     fetchUrl = await get_data(img)
